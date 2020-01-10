1b88bfabc32bc91e52a55d0941483eb5ad83d4ab
Katarina Boskovic <katarina.boskovic@alphasights.com>
2020-01-10 14:27:05 +0000

    adding sign in and registration

--- React/image_recognition/src/App.js
+++ React/image_recognition/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import Rank from './components/Rank/rank';
+import FaceRecognition from './components/FaceRecognition/face_recognition';
+import SignIn from './components/SignIn/sign_in';
+import Register from './components/Register/register';
+
 
 import ImageLinkForm from './components/ImageLinkForm/image_link_form';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '19d678766c8e4cb4ac8dd0009b4c5b71'
+ });
+ 
 
 const particlesOptions = {
   particles: {
@@ -21,16 +31,72 @@ const particlesOptions = {
 }
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (response) => {
+    const detectedFace = response.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const imageWidth = Number(image.width);
+    const imageHeight = Number(image.height);
+
+    return {
+      leftCol: detectedFace.left_col * imageWidth,
+      topRow: detectedFace.top_row * imageHeight,
+      rightCol: imageWidth - detectedFace.right_col * imageWidth,
+      bottomRow: imageHeight - detectedFace.bottom_row * imageHeight
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)    // can't use imageUrl instead of input here - will get an error because setState() is async, updating the state is not done if you call with imageUrl
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(error => console.log(error));
+  }
+
+  onRouteChange = (route) =>{
+    if(route === 'register' || route === 'signin'){
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/* {<FaceRecognition />} */}
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+        { route === 'home' ?
+          <div> 
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition imageUrl={imageUrl} box={box}/>
+          </div>
+          : (route === 'signin' 
+            ? <SignIn onRouteChange={this.onRouteChange}/> 
+            : <Register onRouteChange={this.onRouteChange} />)
+        }
       </div>
     );
   }

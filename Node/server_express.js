70364887ec34bf354acce1afb4ad1c91794f3859
Katarina Boskovic <katarina.boskovic@alphasights.com>
2020-01-10 14:53:38 +0000

    building a server

---
+++ Node/server_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+// middleware - user postman to send a request
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('test'); 
+});
+
+app.get('/profile', (req, res) => {
+  res.send('getting profile'); 
+});
+
+app.post('/profile', (req,res) => {
+  console.log(req.body);
+  const user = {
+    name: 'Sally',
+    hobby: 'football'
+  }
+  // res.send(user); // express automatically does json stringify
+  res.send('success');
+})
+
+app.listen(3002);

1dc1d09570997b28ae52a96f67e56846ae11d00d
Katarina Boskovic <katarina.boskovic@alphasights.com>
2020-02-05 17:03:07 +0000

    adding endpoints

--- React/image_recognition_api/server.js
+++ React/image_recognition_api/server.js
@@ -1,23 +1,89 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '100',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '101',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'apples',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error signing in');
+  }
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password } = req.body
+
+  database.users.push({
+    id: '102',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(404).json('no such user');
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(404).json('no such user');
+  }
 })
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
 })
-
-
-/*
-
-routes
-/ --> res = this is working
-/signin --> POST = with success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT = updated user
-
-*/

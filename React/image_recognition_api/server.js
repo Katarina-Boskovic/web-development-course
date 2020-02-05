1e59f357c32b8a3f6c676af6544ce0a3ff9ca562
Katarina Boskovic <katarina.boskovic@alphasights.com>
2020-02-05 16:27:04 +0000

    image recognition API setup

--- React/image_recognition_api/server.js
+++ React/image_recognition_api/server.js
@@ -9,3 +9,15 @@ app.get('/', (req, res) => {
 app.listen(3001, () => {
   console.log('app is running on port 3001');
 })
+
+
+/*
+
+routes
+/ --> res = this is working
+/signin --> POST = with success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = updated user
+
+*/
